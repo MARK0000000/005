0d40c251f815a33436bebe649fb2b6aae1892821
MARK0000000 <105208112+MARK0000000@users.noreply.github.com>
2023-08-24 12:52:18 +0300

    21

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,32 @@ function menuOpen(event){
 
 }
 
+
+
+const categories = document.querySelector(".aside__categories");
+const recent = document.querySelector(".aside__recent");
+const tags = document.querySelector(".aside__tags");
+const categoriesButton = document.querySelector(".aside__show-button_categories");
+const recentButton = document.querySelector(".aside__show-button_recent");
+const tagsButton = document.querySelector(".aside__show-button_tags");
+
+
+document.addEventListener("click", show)
+
+function show(event){
+   if (event.target.closest(".aside__show-button_categories")) {
+      categories.classList.toggle("aside__categories--active")
+      categoriesButton.classList.toggle("aside__show-button_categories--active")
+   }
+   if (event.target.closest(".aside__show-button_recent")) {
+      recent.classList.toggle("aside__recent--active")
+      recentButton.classList.toggle("aside__show-button_recent--active")
+   }
+   if (event.target.closest(".aside__show-button_tags")) {
+      tags.classList.toggle("aside__tags--active")
+      tagsButton.classList.toggle("aside__show-button_tags--active")
+   }
+
+}
+
+
