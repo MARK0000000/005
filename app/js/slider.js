d84039195d1c4264e3bb82b48bc4d95f0665c98f
Mark <105208112+MARK0000000@users.noreply.github.com>
2024-09-25 10:35:03 +0300

    Add files via upload

---
+++ app/js/slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+ });
+const swiper1 = new Swiper('.swiperTop', {
+   grabCursor: true,
+});
+const swiper2 = new Swiper('.swiperContact', {
+   centeredSlides: true,
+   autoplay: {
+     delay: 3000,
+     disableOnInteraction: false,
+   },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+
+});
+const swiper3 = new Swiper('.swiperBlog', {
+   grabCursor: true,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+ 
+ });
